768849008c72d9201d0175d016e1c1e9e9e84631
Luiz Ferraz <luiz@lferraz.com>
2025-02-08 10:09:37 -0300

    Auto-update versions

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,14 +1,41 @@
-import { readFileSync, writeFileSync } from 'node:fs';
+#!./node_modules/.bin/zx
 
-const targetVersion = process.env.npm_package_version;
+import { readFileSync, writeFileSync } from "node:fs";
+import { $, question } from "zx";
+import * as semver from "semver";
+
+const packageJson = JSON.parse(readFileSync("package.json", "utf8"));
+const suggestedNextVersion = semver.inc(packageJson.version);
+
+const targetVersion =
+	(await question(
+		`Enter target version (default: ${suggestedNextVersion}): `,
+	)) || suggestedNextVersion;
+
+// update package.json with target version
+packageJson.version = targetVersion;
+writeFileSync("package.json", JSON.stringify(packageJson, null, "\t"));
 
 // read minAppVersion from manifest.json and bump version to target version
-const manifest = JSON.parse(readFileSync('manifest.json', 'utf8'));
+const manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
-writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'));
+writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
 // update versions.json with target version and minAppVersion from manifest.json
-const versions = JSON.parse(readFileSync('versions.json', 'utf8'));
+const versions = JSON.parse(readFileSync("versions.json", "utf8"));
 versions[targetVersion] = minAppVersion;
-writeFileSync('versions.json', JSON.stringify(versions, null, '\t'));
+writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+
+// Update lockfile
+await $`pnpm install`;
+
+// Add changes to git
+await $`git add package.json pnpm-lock.yaml manifest.json versions.json`;
+
+// Commit changes
+await $`git commit -m "v${targetVersion}" -- package.json pnpm-lock.yaml manifest.json versions.json`;
+
+await $`git tag ${targetVersion} --sign --message "Release v${targetVersion}"`;
+
+await $`git push --tags`;
